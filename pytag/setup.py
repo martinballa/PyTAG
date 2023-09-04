bd4d8f219f96c4b12911d1336bcd64c1585125c9
martinballa <ballamartin95@gmail.com>
2023-09-04 14:06:30 +0100

    Fixed setup.py to copy all the jar files for full pip install

--- pytag/setup.py
+++ pytag/setup.py
@@ -4,7 +4,7 @@ setup(
     name="pytag",
     version="0.1",
     packages=find_packages(),
-    package_data={'': ['jars/ModernBoardGame.jar']},
+    package_data={'': ['jars/*.jar']},
     include_package_data=True,
     install_requires=["gymnasium", "Jpype1", "numpy", "gdown"],
     extras_require={
